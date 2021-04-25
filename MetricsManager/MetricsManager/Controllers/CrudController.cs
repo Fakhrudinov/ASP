9527c2349cc55d67bb545db11488bdfb704c9b42
Alexander Fakhrudinov <asbuka@gmail.com>
2021-04-25 06:53:11 +0300

    FIX update - not found return BadRequest();

--- MetricsManager/MetricsManager/Controllers/CrudController.cs
+++ MetricsManager/MetricsManager/Controllers/CrudController.cs
@@ -80,7 +80,7 @@ namespace MetricsManager.Controllers
                 }
 
                 if (!founded)
-                    return NoContent();
+                    return BadRequest();
 
                 return Ok();
             }
